774f97770796eaa4d92e727f5f16586ff5f1d706
Ulisses85 <lukasz.kuligowski@gmail.com>
2017-03-11 18:16:21 +0000

    [task1]- array of peaople functionality route

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // call the packages we need
 const express = require('express');
 const app = express();
+const request = require('superagent');
 
 // importing config.js
-const {PORT} = require('./config');
+const {PORT, API} = require('./config');
 
-// initial endpoint setup
-app.get('/', function (request, response, next) {
-  response.send('Hello World!');
+// importing controls for routes
+const {People} = require('./controllers');
+
+//***********1 task- display array of people***********//
+app.get('/people', function (req, res, next) {
+  request
+  .get(`${API}/people`)
+  .end((error, people) => {
+    if (error) console.log(error)
+    else res.send(People.peopleData(people.body));
+  });
 });
 
 app.listen(PORT, function () {
