5072c6d30089e96b6e23f8f665de7046041c9068
Ulisses85 <lukasz.kuligowski@gmail.com>
2017-03-11 18:56:17 +0000

    added asyn for asynchronous call for las t task

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const app = express();
 const request = require('superagent');
-
+const async = require('async');
 // importing config.js
 const {PORT, API} = require('./config');
 
 // importing controls for routes
-const {People, Planet, Films} = require('./controllers');
+const {People, Planet, Films, Vehicles} = require('./controllers');
 
 //***********1 task- display array of people***********//
 app.get('/people', function (req, res, next) {
@@ -42,6 +42,28 @@ app.get('/people', function (req, res, next) {
       if (error) console.log(error);
       else {
         res.status(200).send(Films.directors(films.body));
+      }
+    });
+  });
+
+  //***********task 4 directors and films ***********//
+   app.get('/vehicles', function (req, res, next) {
+    request
+    .get(`${API}/people/1`)
+    .end((err, person) => {
+      if (err) console.log(err);
+      else {
+        async.map(person.body.vehicles, function (link, callback) {
+          request
+          .get(link)
+          .end((err, vehicle) => {
+            if (err) callback(err);
+            else callback(null, vehicle.body);
+          });
+        }, function (err, vehicle) {
+          if (err) return err;
+          res.status(200).send(Vehicles.personVehicle(person.body, vehicle));
+        });
       }
     });
   });
